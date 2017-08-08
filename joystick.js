53dcfac164ab2c1aa2ed0aa1255a37ccb3f734bc
洪文乐 <noclip@foxmail.com>
2017-08-08 16:20:32 +0800

    英雄移动

--- joystick.js
+++ joystick.js
@@ -3,21 +3,38 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
     create: create,
     update: update
 });
-var pad, stick, buttonA;
+var pad, stick, buttonA, hero;
 function preload () {
     game.load.atlas('generic', 'images/generic-joystick.png', 'images/generic-joystick.json');
+    game.load.image('hero', 'images/star.png');
 }
 function create () {
+    // 插件
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     pad = game.plugins.add(Phaser.VirtualJoystick);
+
+    // 摇杆
     stick = pad.addStick(0, 0, 150, 'generic');
-    stick.alignBottomLeft(20);
+    stick.alignBottomLeft();
     stick.scale = 0.5;
-    stick.posX = 100;
-    stick.posY = 520;
+    // 按钮
     buttonA = pad.addButton(700, 520, 'generic', 'button1-up', 'button1-down');
     buttonA.onDown.add(pressButtonA, this);
+    // 英雄
+    hero = game.add.sprite(game.world.width/2, game.world.height/2, 'hero');
+    game.physics.arcade.enable(hero);
 }
-function update () {}
+function update () {
+    if (stick.isDown) {
+        game.physics.arcade.velocityFromRotation(
+            stick.rotation,
+            400,
+            hero.body.velocity
+        );
+    } else {
+        hero.body.velocity.set(0);
+    }
+}
 function pressButtonA () {
     console.log('A');
 }
