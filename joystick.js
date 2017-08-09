09c9898ce6b2e13df508921db07f4cde42c2799a
洪文乐 <noclip@foxmail.com>
2017-08-09 11:13:40 +0800

    全屏、地图、APP

--- joystick.js
+++ joystick.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+var game = new Phaser.Game('100%', '100%', Phaser.AUTO, '', {
     preload: preload,
     create: create,
     update: update
@@ -6,8 +6,8 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 var pad, stick, buttonA, hero, terrains;
 function preload () {
     game.load.atlas('generic', 'images/generic-joystick.png', 'images/generic-joystick.json');
-    game.load.image('hero', 'images/star.png');
-    game.load.image('background','images/background.jpg');
+    game.load.spritesheet('hero', 'images/timg.png', 100, 100);
+    game.load.image('background','images/map.jpg');
     game.load.image('glados','images/glados.png');
 }
 function create () {
@@ -16,15 +16,18 @@ function create () {
     pad = game.plugins.add(Phaser.VirtualJoystick);
     
     // 设置背景、边界
-    game.add.tileSprite(0, 0, 1537, 1537, 'background');
-    game.world.setBounds(0, 0, 1537, 1537);
+    game.add.tileSprite(0, 0, 3200, 2400, 'background');
+    game.world.setBounds(0, 0, 3200, 2400);
 
     // 摇杆
     stick = pad.addStick(0, 0, 150, 'generic');
     stick.alignBottomLeft();
     stick.scale = 0.5;
+    stick.posX = 80;
+    stick.posY = game.camera.view.height - 80;
     // 按钮
-    buttonA = pad.addButton(700, 520, 'generic', 'button1-up', 'button1-down');
+    buttonA = pad.addButton(game.camera.view.width - 80, game.camera.view.height - 80, 'generic', 'button1-up', 'button1-down');
+    buttonA.scale = 0.7;
     buttonA.onDown.add(pressButtonA, this);
 
     // 地形
@@ -39,6 +42,8 @@ function create () {
     hero = game.add.sprite(50, game.world.height - 50, 'hero');
     game.physics.arcade.enable(hero);
     hero.body.collideWorldBounds = true;
+    hero.scale.set(0.8);
+    hero.animations.add('run', [0,1,2,3,4,5,6,7,8,9,10,11], 10, true);
 
     game.camera.follow(hero);
 }
@@ -50,8 +55,10 @@ function update () {
             400,
             hero.body.velocity
         );
+        hero.animations.play('run');
     } else {
         hero.body.velocity.set(0);
+        hero.animations.stop();
     }
 }
 function pressButtonA () {
